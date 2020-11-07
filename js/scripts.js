9db4ce54cb13b6636b660c0e5c6662aba7a05cf4
radeau <kevinradp@gmail.com>
2020-11-07 22:48:43 +0400

    Assignment Week 4

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,12 @@ $(document).ready(function(){
             $("#carouselButton").children("span").addClass('fa-pause');                    
         }
     });
+
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('toggle');
+         });
+        $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show')
+        });
+
 });
